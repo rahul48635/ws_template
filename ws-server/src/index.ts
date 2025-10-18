33a96aa6eaa53cf1c9d7fd6cb338978292c71829
rahul48635 <rahulsingh48635@gmail.com>
2025-10-18 23:26:50 +0530

    changed server to a chat app

--- ws-server/src/index.ts
+++ ws-server/src/index.ts
@@ -1,12 +1,46 @@
-import  WebSocket, { WebSocketServer } from 'ws'
+import WebSocket, { WebSocketServer } from "ws";
 
 const wss=new WebSocketServer({port:8080});
-const arr:WebSocket[]=[]
+const rooms:Record<string,WebSocket[]>={};
+let currentRoomId:string | null=null;
 wss.on('connection',function(ws){
-    arr.push(ws);
-    ws.on('error',console.error);
-    ws.send('Welcome to the ws server');
     ws.on('message',function(data){
-        arr.forEach(ws=>ws.send(data.toString()));
+        const msg=JSON.parse(data.toString());
+        if(msg.type==="JOIN"){
+            const {roomId}=msg;
+            currentRoomId=roomId;
+            if(Object.hasOwn(rooms,roomId)){
+                rooms[roomId]?.push(ws);
+            }else{
+                rooms[roomId]=[];
+                rooms[roomId].push(ws);
+            }
+            ws.send(`Joined Room: ${roomId}`)
+        }
+
+        if(msg.type==="CHAT"){
+            if(currentRoomId){
+                rooms[currentRoomId]?.forEach((c)=>{
+                    if(c!==ws && c.readyState===WebSocket.OPEN){
+                        c.send(msg.payload);
+                    }
+                })
+            }
+        }
+
+        ws.on('close',function(){
+            if(currentRoomId){
+                const clients = rooms[currentRoomId];
+                if (clients) {
+                    const index = clients.indexOf(ws);
+                    if (index > -1) {
+                        clients.splice(index, 1);
+                    }
+                    if (clients.length === 0) {
+                        delete rooms[currentRoomId];
+                    }
+                }
+            }
+        })
     })
 })
